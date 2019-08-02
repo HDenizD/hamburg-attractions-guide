aa5d25f72c210b22b43de7241352f1609a02e530
Deniz <denizderezzed@gmail.com>
2019-08-02 18:54:17 +0200

    changed map style

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,7 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGVuaXp6ZWQiLCJhIjoiY2p5dTQyb2UyMGExNDNocDgxZ3R0YzE0YSJ9.coHuDe_l_8rWRoTBW9u8SQ';
 var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
-  center: [-77.38, 39], // starting position
-  zoom: 3 // starting zoom
+container: 'map',
+style: 'mapbox://styles/denizzed/cjyu2kr5e0rqq1cry6wmo3rhg'
 });
