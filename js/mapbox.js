2771f54e1712a7db552df318b6f0450d3a6839e9
Deniz <denizderezzed@gmail.com>
2019-08-04 11:46:20 +0200

    about to set all cooridnates

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,30 +1,136 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGVuaXp6ZWQiLCJhIjoiY2p5dTQyb2UyMGExNDNocDgxZ3R0YzE0YSJ9.coHuDe_l_8rWRoTBW9u8SQ';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/denizzed/cjyu2kr5e0rqq1cry6wmo3rhg',
-center: [10.0009,53.5476],
-zoom: 11.41,
+  container: 'map',
+  style: 'mapbox://styles/denizzed/cjyu2kr5e0rqq1cry6wmo3rhg',
+  center: [10.0009, 53.5476],
+  zoom: 11.41,
 });
 
-const mapLeaflet = L.mapbox.map('map-leaflet')
-  .setView([37.8, -96], 4)
-  .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
+var geojson = {
+  "type": "FeatureCollection",
+  "features": [{
+      "type": "Feature",
+      "properties": {
+        "message": "Foo",
+        "iconSize": [60, 60]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
 
-L.marker([53.557377, 9.997655]).addTo(mapLeaflet);
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Bar",
+        "iconSize": [50, 50]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Baz",
+        "iconSize": [40, 40]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Bar",
+        "iconSize": [50, 50]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Bar",
+        "iconSize": [50, 50]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Bar",
+        "iconSize": [50, 50]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "message": "Bar",
+        "iconSize": [50, 50]
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.9946585,
+          53.5542614
+        ]
+      }
+    },
+  ]
+};
 
-var hamburgAttractions = [
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [53.557377, 9.997655]
-    }
-  },
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.413682, 37.775408]
-    }
-  }
-];
+geojson.features.forEach(function(marker) {
+  // create a DOM element for the marker
+  var el = document.createElement('div');
+  el.className = 'marker';
+  el.style.backgroundImage = 'url(https://placekitten.com/g/' + marker.properties.iconSize.join('/') + '/)';
+  el.style.width = marker.properties.iconSize[0] + 'px';
+  el.style.height = marker.properties.iconSize[1] + 'px';
+
+  el.addEventListener('click', function() {
+    window.alert(marker.properties.message);
+  });
+
+  // add marker to map
+  new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .addTo(map);
+});
+
+
+// type: 'Feature',
+//   geometry: {
+//     type: 'Point',
+//     coordinates: [53.557377, 9.997655]
+//   }
