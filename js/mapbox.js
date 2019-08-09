b2c217116a765b92a81663b6536fdbe723aabd99
Deniz <denizderezzed@gmail.com>
2019-08-09 16:09:33 +0200

    sidebar toggle works now

--- js/mapbox.js
+++ js/mapbox.js
@@ -316,10 +316,17 @@ flyToLocation('.btn-fischmarkt',
   lngLatLocation.fischmarkt[1]);
 
 
-const sideBar = document.querySelector('#sidebar');
-sideBar.addEventListener('click', () => {
-  // ADD TRANSITION
-  sideBar.style.left = '-300px';
+// SIDEBAR TOGGLE ANIMATION
+const sidebar = document.querySelector('#sidebar');
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+sidebarToggle.addEventListener('click', () => {
+  if (sidebar.style.left == '-300px') {
+    sidebarToggle.innerHTML = '<';
+    sidebar.style.left = '0px';
+  } else {
+    sidebarToggle.innerHTML = '>';
+    sidebar.style.left = '-300px';
+  }
 });
 
 
