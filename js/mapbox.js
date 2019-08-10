d914fed560d0ad82990a57787ed4fb26e9ed2173
Deniz <denizderezzed@gmail.com>
2019-08-10 08:23:10 +0200

    code cleaning: line width 80 chars

--- js/mapbox.js
+++ js/mapbox.js
@@ -269,7 +269,8 @@ function loadLocations() {
 }
 
 // AUTENTIFICATION AND META DATA FOR MAPBOX
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGVuaXp6ZWQiLCJhIjoiY2p5dTQyb2UyMGExNDNocDgxZ3R0YzE0YSJ9.coHuDe_l_8rWRoTBW9u8SQ';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZGVuaXp6ZWQiLCJhIjoiY2p5dTQyb2UyMGExNDNocDgxZ3R0YzE0YSJ9.coHuDe_l_8rWRoTBW9u8SQ';
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/denizzed/cjyu2kr5e0rqq1cry6wmo3rhg',
