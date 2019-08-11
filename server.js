390a2c64fa2aa88c080c6b5367a376d09fdc8d7e
Deniz <denizderezzed@gmail.com>
2019-08-11 17:36:48 +0200

    changed console msg

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(
-    `Example app listening on port localhost:${port}`);
+    `App listening on port localhost:${port}`);
 });
