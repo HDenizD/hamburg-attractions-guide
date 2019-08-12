c5aeda29505c6103b8911948f8c7ef0c1de49a8f
Deniz <denizderezzed@gmail.com>
2019-08-12 06:51:48 +0200

    made sidebar scrollable and fixed sidebar toggle button

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,15 @@ const sidebar = document.querySelector('#sidebar');
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 
 sidebarToggle.addEventListener('click', () => {
+
+  console.log(sidebarToggle.style.left);
   if (sidebar.style.left == '-300px') {
     sidebarToggle.innerHTML = '<';
     sidebar.style.left = '0px';
+    sidebarToggle.style.left = '299px';
   } else {
     sidebarToggle.innerHTML = '>';
     sidebar.style.left = '-300px';
+    sidebarToggle.style.left = '-1px';
   }
 });
